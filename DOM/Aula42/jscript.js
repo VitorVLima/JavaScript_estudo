4fa7281e99c1d0f96cdc4feb68eaba9a336706d8
VITOR VINICIUS LIMA SILVA <vitor14_df@hotmail.com>
2025-01-21 22:18:10 -0300

    vitor

--- DOM/Aula42/jscript.js
+++ DOM/Aula42/jscript.js
@@ -1,3 +1,12 @@
-var nome = 'vito'
+const idades = [15,34,32,12,64,23,28,21,64,32,11]
 
-console.log(nome)
+const maior = idades.filter((valor)=>{
+        if(valor>=18){
+            return valor
+        }
+})
+
+
+
+console.log(idades)
+console.log(maior)
