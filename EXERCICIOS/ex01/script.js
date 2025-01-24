3c53f5aca97b17186ba7dae67019f76c8b871d81
VITOR VINICIUS LIMA SILVA <vitor14_df@hotmail.com>
2025-01-24 14:23:25 -0300

    viotor

--- EXERCICIOS/ex01/script.js
+++ EXERCICIOS/ex01/script.js
@@ -2,6 +2,37 @@ const items = [...document.querySelectorAll("li")];
 const searchButton = document.querySelector("#btnPesquisar");
 const txt_digitado = document.querySelector("#pesquisa");
 const category = document.querySelector("#categoriaSelect");
+const botaoCarrinho = document.getElementById("botaoCarrinho")
+const fecharCarrinho = document.getElementById("fecharCarrinho")
+const caixaCarrinho = document.getElementById("modalCarrinho")
+const Carrinho = document.getElementById("itensCarrinho")
+let contadorCarrinho = 0
+
+function gerarPreçoQuantidade(){
+    const preco = (Math.random() * (20 - 5) + 5).toFixed(2)
+    const quantidade = Math.floor(Math.random() *100)
+    return {preco,quantidade}
+}
+
+items.map((el)=>{
+    const {preco, quantidade} = gerarPreçoQuantidade()
+    const precoElement = el.querySelector('.preco');
+    const quantidadeElement = el.querySelector('.quantidade');
+    precoElement.textContent = `Preço: R$ ${preco}`;
+    quantidadeElement.textContent = `Quantidade em Estoque: ${quantidade}`;
+    const botaoComprar = document.createElement("button")
+    botaoComprar.setAttribute("class", "botaoComprar")
+    botaoComprar.innerHTML = "Comprar"
+    el.appendChild(botaoComprar)
+
+    botaoComprar.addEventListener("click", () => {
+        contadorCarrinho = contadorCarrinho + 1
+        const itemCarrinho = document.createElement("li");
+        itemCarrinho.setAttribute("class", "item");
+        itemCarrinho.innerHTML = el.querySelector('span').previousSibling.textContent; // Nome do produto
+        Carrinho.appendChild(itemCarrinho);
+        botaoCarrinho.innerHTML = "&#128722; " + contadorCarrinho})
+})
 
 searchButton.addEventListener("click", () => {
     const itemDigitado = txt_digitado.value.toLowerCase().trim();
@@ -17,7 +48,7 @@ searchButton.addEventListener("click", () => {
     }
 
     items.map((el) => {
-        const item = el.textContent.toLowerCase();
+        const item = el.querySelector('span').previousSibling.textContent.toLowerCase();
         const categoria = el.getAttribute("data-categoria").toLowerCase();
 
         // Verifica se o item corresponde à categoria e à pesquisa
@@ -34,6 +65,18 @@ searchButton.addEventListener("click", () => {
         }
     });
 });
+
+botaoCarrinho.addEventListener("click",()=>{
+    caixaCarrinho.style.display = "block"
+    contadorCarrinho = 0
+    botaoCarrinho.innerHTML = "&#128722;"
+})
+
+fecharCarrinho.addEventListener("click",()=>{
+    caixaCarrinho.style.display = "none"
+})
+
+
 
 // searchButton.addEventListener("click",(evt)=>{
 //     const valor = items.find((el)=>{
